8cb7b5f903d14c0a5dc31d3eb04952c3b50b9c73
kruz <lyghtkruz@gmail.com>
2024-12-28 22:45:33 -0600

    adding addons, classes and temp assets
    Adding TODO and Logging addons
    Adding base plant and clickable item classes
    Adding temporary assets

---
+++ plants/plant.gd
@@ -0,0 +1,9 @@
+class_name Plants
+extends Resource
+
+@export var name: String = "Unnamed Plant"
+@export var hit_points: int = 6
+@export var hit_points_regen: int = 20
+@export var projectile_path: String = "none"
+@export var sun_cost: int = 50 
+@export var dig_cost: int = 25
