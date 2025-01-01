43a94c285e0997336448e59e6993f50d940d4d33
kruz <lyghtkruz@gmail.com>
2024-12-31 21:06:07 -0600

    added sunflower
    initial sunflower and parabola spawn, also added the ability the set up the UI with resources... may need to find a way to combine them to have less complexity

---
+++ ui/cards.gd
@@ -0,0 +1,7 @@
+class_name Cards
+extends Resource
+
+@export var plant_name: String = "Unnamed plant"
+@export var plant_scene: PackedScene
+@export var card_back: Texture2D
+@export var plant_texture: Texture2D
